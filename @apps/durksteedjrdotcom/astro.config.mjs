f6ced60b38d18bac5668b6787d9940761302ce23
Durk Steed Jr <139193744+durksteedjr@users.noreply.github.com>
2024-07-16 23:53:33 -0400

    fix: lint

--- @apps/durksteedjrdotcom/astro.config.mjs
+++ @apps/durksteedjrdotcom/astro.config.mjs
@@ -1,11 +1,11 @@
 import react from "@astrojs/react";
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 const config = defineConfig({
   integrations: [react()],
   server: {
-    port: 3000
-  }
+    port: 3000,
+  },
 });
 
 export default config;
