38133f13da278daaf2c3c4fbc3d6f8f64ba26c92
Durk Steed Jr <139193744+durksteedjr@users.noreply.github.com>
2024-07-17 00:07:17 -0400

    feat: add tailwind

--- @packages/ui/src/components/Button/Button.tsx
+++ @packages/ui/src/components/Button/Button.tsx
@@ -3,5 +3,7 @@ import type { HTMLAttributes } from "react";
 export type ButtonProps = HTMLAttributes<HTMLButtonElement>;
 
 export const Button = ({ children, ...props }: ButtonProps) => (
-  <button {...props}>{children}</button>
+  <button className="bg-red-50" {...props}>
+    {children}
+  </button>
 );
